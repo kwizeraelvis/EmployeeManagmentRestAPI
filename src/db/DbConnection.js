358b815dfd6266455a35d42d877f4bd1e93f304b
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-13 01:05:37 +0200

    Heroku build bug fix attempt #1

--- src/db/DbConnection.js
+++ src/db/DbConnection.js
@@ -1,9 +1,13 @@
 import {Pool} from 'pg'
 import 'dotenv/config';
 
-const isProduction = process.env.NODE_ENV === 'production'
-const connectionString = process.env.DEV_DATABASE_URL
+let pool;
+if(process.env.NODE_ENV === 'development'){
+    pool = new Pool({connectionString: process.env.DEV_DATABASE_URL})
+}
 
-const pool = new Pool({connectionString: isProduction ? process.env.DATABASE_URl: connectionString})
+if(process.env.NODE_ENV === 'production'){
+    pool = new Pool({connectionString: process.env.DATABASE_URL})
+}
 
-export {pool};
+export { pool }
