c7a4a0056dff8b07ea06b722197073d23d7741b7
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-13 01:20:38 +0200

    Folder restructure to avoid missing folder error in herokubuild(Heroku build bug fix attempt #2)

--- src/helpers/DataPath.js
+++ src/helpers/DataPath.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 const publicDir = path.join(__dirname, '../../public/css');
 const uploadDir = path.join(__dirname, '../../uploads/');
-const viewDir = path.join(__dirname, '../templates/views');
+const viewDir = path.join(__dirname, '../../templates/views');
 
 export {
     publicDir,
