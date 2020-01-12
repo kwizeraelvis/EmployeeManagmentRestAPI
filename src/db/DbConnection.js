4661cfb52b3d75e76ebb2c7a3cd0d26d3624063d
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-01-12 23:44:23 +0200

    Tasks accomplished in this commit ---------------------------------- 1. Dbconnection bug fix

--- src/db/DbConnection.js
+++ src/db/DbConnection.js
@@ -1,15 +1,9 @@
 import {Pool} from 'pg'
 import 'dotenv/config';
 
+const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = process.env.DEV_DATABASE_URL
 
-let pool;
-
-if(process.env.NODE_ENV === 'development'){
-    pool = new Pool({connectionString: process.env.DEV_DATABASE_URL});
-}
-
-if(process.env.NODE_ENV === 'production'){
-    pool = new Pool({connectionString: process.env.DATABASE_URL});
-}
+const pool = new Pool({connectionString: isProduction ? process.env.DATABASE_URl: connectionString})
 
 export {pool};
